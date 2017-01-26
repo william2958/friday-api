998d48c877c7f4d9b091251196f260fd2fe6643d
William <william2958@betatest.com>
2017-01-26 13:53:47 -0500

    Added ENV variables

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,9 +69,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            'crimsonlockconfirmation@gmail.com',
-    password:             '#Cocacola101',
+    domain:               ENV["GMAIL_DOMAIN"],
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  
   }

546a1b425a0f6a48d2309c6f727fd381e56bea6d
William <william2958@betatest.com>
2017-02-05 03:04:36 -0500

    Beta 1.7.3
    Revert back to original mailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,21 +66,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "https://assistantfriday.com" }
 
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               ENV["GMAIL_DOMAIN"],
-  #   user_name:            ENV["GMAIL_USERNAME"],
-  #   password:             ENV["GMAIL_PASSWORD"],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  
-  # }
-
   config.action_mailer.smtp_settings = {
-    address:              'smtp.zoro.com',
+    address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'assistantfriday.com',
-    user_name:            'no-reply@assistantfriday.com',
+    domain:               ENV["GMAIL_DOMAIN"],
+    user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  
